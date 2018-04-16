67ae6b83a62d8c54cefc73a35668ae844b8f18c7
Dillon Welch <daw0328@gmail.com>
2018-04-16 11:29:04 -0700

    Fix bad merge conflict resolution

--- test/dummy/app/models/secure_user.rb
+++ test/dummy/app/models/secure_user.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
 class SecureUser < ApplicationRecord
->>>>>>> origin/master
   devise :database_authenticatable, :secure_validatable, email_validation: false
 end
--- test/dummy/app/models/security_question_user.rb
+++ test/dummy/app/models/security_question_user.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class SecurityQuestionUser < ApplicationRecord
->>>>>>> origin/master
   self.table_name = 'users'
   devise :database_authenticatable, :password_archivable, :lockable,
          :paranoid_verification, :password_expirable, :security_questionable
