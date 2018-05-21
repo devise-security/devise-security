84d4e2cfc325ce98c84c1ad8609a193d92bc0a30
scicco <zendar79@gmail.com>
2018-05-21 10:34:54 +0200

    version check syntax updated

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,7 +13,7 @@ require 'devise-security'
 
 ActiveRecord::Migration.verbose = false
 ActiveRecord::Base.logger = Logger.new(nil)
-if Rails.version >= '5.2.0'
+if Rails.gem_version >= Gem::Version.new('5.2.0')
   ActiveRecord::MigrationContext.new(File.expand_path('../dummy/db/migrate', __FILE__)).migrate
 else
   ActiveRecord::Migrator.migrate(File.expand_path('../dummy/db/migrate', __FILE__))
