6ebcc25a3bcb6af7a4d579975eb2ab5553673a68
Kevin Olbrich <kevin.olbrich@mckesson.com>
2018-06-21 16:58:19 -0400

    Rubocop autocorrect

--- lib/devise-security/models/password_expirable.rb
+++ lib/devise-security/models/password_expirable.rb
@@ -4,7 +4,6 @@ require 'devise-security/hooks/password_expirable'
 
 module Devise
   module Models
-
     # PasswordExpirable takes care of change password after
     module PasswordExpirable
       extend ActiveSupport::Concern
@@ -16,7 +15,7 @@ module Devise
       # is an password change required?
       def need_change_password?
         if expired_password_after_numeric?
-          self.password_changed_at.nil? || self.password_changed_at < self.expire_password_after.seconds.ago
+          password_changed_at.nil? || password_changed_at < expire_password_after.seconds.ago
         else
           false
         end
@@ -26,20 +25,20 @@ module Devise
       def need_change_password!
         if expired_password_after_numeric?
           need_change_password
-          self.save(validate: false)
+          save(validate: false)
         end
       end
 
       # set a fake datetime so a password change is needed
       def need_change_password
         if expired_password_after_numeric?
-          self.password_changed_at = self.expire_password_after.seconds.ago
+          self.password_changed_at = expire_password_after.seconds.ago
         end
 
         # is date not set it will set default to need set new password next login
-        need_change_password if self.password_changed_at.nil?
+        need_change_password if password_changed_at.nil?
 
-        self.password_changed_at
+        password_changed_at
       end
 
       def expire_password_after
@@ -48,22 +47,20 @@ module Devise
 
       private
 
-        # is password changed then update password_cahanged_at
-        def update_password_changed
-          self.password_changed_at = Time.now if (self.new_record? || self.encrypted_password_changed?) && !self.password_changed_at_changed?
-        end
+      # is password changed then update password_cahanged_at
+      def update_password_changed
+        self.password_changed_at = Time.now if (new_record? || encrypted_password_changed?) && !password_changed_at_changed?
+      end
 
-        def expired_password_after_numeric?
-          return @_numeric if defined?(@_numeric)
-          @_numeric ||= self.expire_password_after.is_a?(1.class) ||
-            self.expire_password_after.is_a?(Float)
-        end
+      def expired_password_after_numeric?
+        return @_numeric if defined?(@_numeric)
+        @_numeric ||= expire_password_after.is_a?(1.class) ||
+                      expire_password_after.is_a?(Float)
+      end
 
       module ClassMethods
         ::Devise::Models.config(self, :expire_password_after)
       end
     end
-
   end
-
 end
