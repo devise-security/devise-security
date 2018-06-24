e7b32bde99bcad289498df89610f870c12602246
Kevin Olbrich <kevin.olbrich@mckesson.com>
2018-06-24 11:11:23 -0400

    refactor DSL for clarity and simplicity

--- lib/devise-security/models/password_expirable.rb
+++ lib/devise-security/models/password_expirable.rb
@@ -22,44 +22,57 @@ module Devise
       # @return [true] if +password_changed_at+ has not been set or if it is old
       #   enough based on +expire_password_after+ configuration.
       def need_change_password?
-        if enabled?
-          if expire_on_demand?
-            # Only expire when password_changed_at is nil
-            password_changed_at.nil?
-          else
-            # Expire when password_changed_at is nil or when the last time the
-            # password was changed is sufficiently old
-            password_changed_at.nil? || password_changed_at < expire_password_after.seconds.ago
-          end
-        else
-          false
-        end
+        password_change_requested? || password_too_old?
       end
 
       # Adjust the +password_changed_at+ field so that it will require a
       # password update and save the record (without validations)
       # @return [Boolean]
       def need_change_password!
-        return unless enabled?
+        return unless password_expiration_enabled?
         need_change_password
         save(validate: false)
       end
+      alias expire_password! need_change_password!
+      alias request_password_change! need_change_password!
 
       # Clear the +password_changed_at+ field so that it will require a
       # password update.
       # @return [void]
       def need_change_password
-        return unless enabled?
+        return unless password_expiration_enabled?
         self.password_changed_at = nil
       end
+      alias expire_password need_change_password
+      alias request_password_change need_change_password
 
       # Set this value to a number of seconds to have passwords expire after a time
       # Set to +true+ if you want to be able to manually expire passwords on demand
       # without a time limit.
-      # @return [Numeric, true]
+      # @return [Numeric, Boolean]
       def expire_password_after
         self.class.expire_password_after
       end
+
+      # When +password_changed_at+ is set to null in the database
+      # the user is required to change their password.  This only happens
+      # on demand or when the column is first added to the table.
+      # @return [Boolean]
+      def password_change_requested?
+        return false unless password_expiration_enabled?
+        return false if new_record?
+        password_changed_at.nil?
+      end
+
+      # Is this password older than the configured expiration timeout
+      # @return [Boolean]
+      def password_too_old?
+        return false if new_record?
+        return false unless password_expiration_enabled?
+        return false if expire_password_on_demand?
+        password_changed_at < expire_password_after.seconds.ago
+      end
+      alias password_expired? password_too_old?
 
       private
 
@@ -71,15 +84,15 @@ module Devise
       end
 
       # Enabled if configuration +expire_password_after+ is set to an {Integer}, {Float}, or {true}
-      def enabled?
-        return @enabled if defined?(@enabled)
-        @enabled = expire_password_after.is_a?(1.class) ||
-                   expire_password_after.is_a?(Float) ||
-                   expire_password_after == true
+      def password_expiration_enabled?
+        expire_password_after.present? &&
+        expire_password_after.is_a?(1.class) ||
+          expire_password_after.is_a?(Float) ||
+          expire_password_on_demand?
       end
 
-      def expire_on_demand?
-        expire_password_after == true
+      def expire_password_on_demand?
+        expire_password_after.present? && expire_password_after == true
       end
 
       module ClassMethods
--- test/test_password_expirable.rb
+++ test/test_password_expirable.rb
@@ -15,35 +15,69 @@ class TestPasswordArchivable < ActiveSupport::TestCase
     Devise.expire_password_after = false
     user = User.create email: 'bob@microsoft.com', password: 'Password1', password_confirmation: 'Password1'
     refute user.need_change_password?
+    refute user.password_expired?
     user.need_change_password!
     refute user.need_change_password?
+    refute user.password_expired?
   end
 
-  test 'password expires on demand' do
+  test 'password change can be requested' do
     Devise.expire_password_after = true
     user = User.create email: 'bob@microsoft.com', password: 'Password1', password_confirmation: 'Password1'
     refute user.need_change_password?
+    refute user.password_expired?
+    refute user.password_change_requested?
     user.need_change_password!
     assert user.need_change_password?
+    refute user.password_expired? # it's not too old because it's not set at all
+    assert user.password_change_requested?
   end
 
   test 'password expires' do
     user = User.create email: 'bob@microsoft.com', password: 'Password1', password_confirmation: 'Password1'
     refute user.need_change_password?
-
+    refute user.password_expired?
+    refute user.password_too_old?
     user.update(password_changed_at: Time.now.ago(3.months))
+    assert user.password_too_old?
     assert user.need_change_password?
+    assert user.password_expired?
+    refute user.password_change_requested?
   end
 
   test 'saving a record records the time the password was changed' do
     user = User.new email: 'bob@microsoft.com', password: 'Password1', password_confirmation: 'Password1'
     assert user.password_changed_at.nil?
+    refute user.password_change_requested?
+    refute user.password_expired?
     user.save
-    refute user.password_changed_at.nil?
+    assert user.password_changed_at.present?
+    refute user.password_change_requested?
+    refute user.password_expired?
+  end
+
+  test 'updating a record updates the time the password was changed if the password is changed' do
+    user = User.create email: 'bob@microsoft.com', password: 'Password1', password_confirmation: 'Password1'
+    user.expire_password!
+    assert user.password_change_requested?
+    user.password = "NewPassword1"
+    user.password_confirmation = "NewPassword1"
+    user.save
+    assert user.previous_changes.key?(:password_changed_at)
+    refute user.password_change_requested?
+  end
+
+  test 'updating a record does not updates the time the password was changed if the password was not changed' do
+    user = User.create email: 'bob@microsoft.com', password: 'Password1', password_confirmation: 'Password1'
+    user.expire_password!
+    assert user.password_change_requested?
+    user.save
+    refute user.previous_changes.key?(:password_changed_at)
+    assert user.password_change_requested?
   end
 
   test 'override expire after at runtime' do
-    user = User.new email: 'bob@microsoft.com', password: 'Password1', password_confirmation: 'Password1'
+    user = User.create email: 'bob@microsoft.com', password: 'Password1', password_confirmation: 'Password1'
     user.instance_eval do
       def expire_password_after
         4.months
@@ -51,7 +85,9 @@ class TestPasswordArchivable < ActiveSupport::TestCase
     end
     user.password_changed_at = Time.now.ago(3.months)
     refute user.need_change_password?
+    refute user.password_expired?
     user.password_changed_at = Time.now.ago(5.months)
     assert user.need_change_password?
+    assert user.password_expired?
   end
 end
