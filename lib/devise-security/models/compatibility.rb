ac689f1de86629bca3181bcb7394dc003cdcd1bc
Kevin Olbrich <kevin.olbrich@mckesson.com>
2019-02-17 22:00:29 -0500

    implement some methods for compatibility between mongoid and active_record

--- lib/devise-security/models/compatibility.rb
+++ lib/devise-security/models/compatibility.rb
@@ -5,18 +5,28 @@ module Devise
     module Compatibility
       extend ActiveSupport::Concern
 
-      # for backwards compatibility with Rails < 5.1.x
-      unless Devise.activerecord51?
-        def saved_change_to_encrypted_password?
-          encrypted_password_changed?
-        end
+      if DEVISE_ORM == :active_record
+        # for backwards compatibility with Rails < 5.1.xi
+        unless Devise.activerecord51?
+          def saved_change_to_encrypted_password?
+            encrypted_password_changed?
+          end
 
-        def encrypted_password_before_last_save
-          previous_changes['encrypted_password'].try(:first)
+          def encrypted_password_before_last_save
+            previous_changes['encrypted_password'].try(:first)
+          end
+
+          def will_save_change_to_encrypted_password?
+            changed_attributes['encrypted_password'].present?
+          end
+        end
+      elsif DEVISE_ORM == :mongoid
+        def will_save_change_to_email?
+          changed.include? 'email'
         end
 
         def will_save_change_to_encrypted_password?
-          changed_attributes['encrypted_password'].present?
+          changed.include? 'encrypted_password'
         end
       end
     end
