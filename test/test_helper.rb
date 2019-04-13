c10e919a8aac47a54cfce4356adca7023a5ec80f
Kevin Olbrich <kevin.olbrich@gmail.com>
2019-04-13 14:14:56 -0400

    Tweak coverage groups

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,9 +6,17 @@ DEVISE_ORM = ENV.fetch('DEVISE_ORM',  'active_record').to_sym
 require 'simplecov'
 SimpleCov.start do
   add_filter 'gemfiles'
+  add_filter 'test/dummy/db'
+  add_group 'ActiveRecord', 'active_record'
+  add_group 'Expirable', /(?<!password_)expirable/
+  add_group 'Mongoid', 'mongoid'
+  add_group 'Paranoid Verifiable', 'paranoid_verification'
+  add_group 'Password Archivable', /password_archivable|old_password/
+  add_group 'Password Expirable', /password_expirable|password_expired/
+  add_group 'Secure Validateable', 'secure_validatable'
+  add_group 'Security Questionable', 'security_question'
+  add_group 'Session Limitable', 'session_limitable'
   add_group 'Tests', 'test'
-  add_group 'Password Archivable', 'password_archivable'
-  add_group 'Password Expirable', 'password_expirable'
 end
 
 if ENV['CI']
