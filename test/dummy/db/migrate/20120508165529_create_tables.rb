a44a1e699c8fc2a9b3dd4a8b298fb2308b1e2357
Kevin Olbrich <kevin.olbrich@gmail.com>
2019-04-16 11:35:34 -0400

    add columns to base migration that were missing (normally these are created by devise)

--- test/dummy/db/migrate/20120508165529_create_tables.rb
+++ test/dummy/db/migrate/20120508165529_create_tables.rb
@@ -14,6 +14,13 @@ class CreateTables < MIGRATION_CLASS
       t.string :encrypted_password, null: false, default: ''
 
       t.datetime :password_changed_at
+
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string :current_sign_in_ip
+      t.string :last_sign_in_ip
+      t.integer :sign_in_count, default: 0
+      t.integer :failed_attempts, default: 0
       t.timestamps null: false
     end
     add_index :users, :password_changed_at
