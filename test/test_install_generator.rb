a14d2047b5d644f1606c2a9b7aade8db03485f48
mahdi alizadeh <mahdializadeh20@gmail.com>
2020-10-08 14:29:32 +0330

    locale alphabetize

--- test/test_install_generator.rb
+++ test/test_install_generator.rb
@@ -15,13 +15,13 @@ class TestInstallGenerator < Rails::Generators::TestCase
     assert_file 'config/locales/devise.security_extension.de.yml'
     assert_file 'config/locales/devise.security_extension.en.yml'
     assert_file 'config/locales/devise.security_extension.es.yml'
+    assert_file 'config/locales/devise.security_extension.fa.yml'
     assert_file 'config/locales/devise.security_extension.fr.yml'
     assert_file 'config/locales/devise.security_extension.it.yml'
     assert_file 'config/locales/devise.security_extension.ja.yml'
-    assert_file 'config/locales/devise.security_extension.tr.yml'
     assert_file 'config/locales/devise.security_extension.nl.yml'
     assert_file 'config/locales/devise.security_extension.pt.yml'
-    assert_file 'config/locales/devise.security_extension.fa.yml'
+    assert_file 'config/locales/devise.security_extension.tr.yml'
     assert_file 'config/locales/devise.security_extension.uk.yml'
   end
 end
