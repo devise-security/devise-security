6805286329b9b8dc3e0de570d0971192173df657
hnatt <hnatt88@gmail.com>
2020-12-22 11:18:50 +0200

    Separate positional and keyword arguments
    Ruby 3.0 will [separate positional and keyword arguments][1], so Ruby
    2.7 gives deprecation warnings about code that will become incompatible:
    
    ```
    devise-security/lib/devise-security/controllers/helpers.rb:91: warning:
    Using the last argument as keyword parameters is deprecated; maybe **
    should be added to the call
    ```
    
    This commit fixes such warnings
    
    [1]: https://www.ruby-lang.org/en/news/2019/12/12/separation-of-positional-and-keyword-arguments-in-ruby-3-0/

--- lib/devise-security/controllers/helpers.rb
+++ lib/devise-security/controllers/helpers.rb
@@ -88,11 +88,11 @@ module DeviseSecurity
 
       # redirect for password update with alert message
       def redirect_for_password_change(scope)
-        redirect_to change_password_required_path_for(scope), alert: I18n.t('change_required', { scope: 'devise.password_expired' })
+        redirect_to change_password_required_path_for(scope), alert: I18n.t('change_required', scope: 'devise.password_expired')
       end
 
       def redirect_for_paranoid_verification(scope)
-        redirect_to paranoid_verification_code_path_for(scope), alert: I18n.t('code_required', { scope: 'devise.paranoid_verify' })
+        redirect_to paranoid_verification_code_path_for(scope), alert: I18n.t('code_required', scope: 'devise.paranoid_verify')
       end
 
       # path for change password
