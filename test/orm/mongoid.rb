e3850bf240c3318010ab97b45540c4b3f01c5676
Nate Bird <natebird@gmail.com>
2021-02-10 17:12:54 -0500

    Try requiring database_cleaner-mongoid

--- test/orm/mongoid.rb
+++ test/orm/mongoid.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'mongoid/version'
+require 'database_cleaner-mongoid'
 
 Mongoid.configure do |config|
   config.load!('test/support/mongoid.yml', Rails.env)
