ca55ff2a9f9c1229befa99bdb632393c27545190
Dillon Welch <daw0328@gmail.com>
2021-03-19 05:26:43 -0400

    Working test

--- test/controllers/test_minimum_password_length_controller.rb
+++ test/controllers/test_minimum_password_length_controller.rb
@@ -7,6 +7,9 @@ class TestWithSecureValidatable < ActionController::TestCase
   tests MinimumPasswordLengthController
 
   test 'When using secure_validatable, @minimum_password_length is set' do
-    assert_equal 'hi', 'hi'
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    # byebug
+    # puts @controller.set_minimum_password_length
+    assert_equal @controller.set_minimum_password_length, Devise.password_length.min
   end
 end
