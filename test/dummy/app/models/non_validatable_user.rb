6a2e8a30ac3ff10fb05c5eebe02083258612c762
Dillon Welch <daw0328@gmail.com>
2021-03-19 17:11:29 -0400

    refactor non_validatable_user

--- test/dummy/app/models/non_validatable_user.rb
+++ test/dummy/app/models/non_validatable_user.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class NonValidatableUser < ApplicationUserRecord
-  devise :database_authenticatable
   if DEVISE_ORM == :mongoid
     require './test/dummy/app/models/mongoid/mappings'
     include ::Mongoid::Mappings
