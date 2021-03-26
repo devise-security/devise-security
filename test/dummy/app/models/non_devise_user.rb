ad2fb36769da2ff177cd22c85e394e989938c5cd
Dillon Welch <daw0328@gmail.com>
2021-03-25 23:20:59 -0400

    Remove mongoid devise ORM for non devise user

--- test/dummy/app/models/non_devise_user.rb
+++ test/dummy/app/models/non_devise_user.rb
@@ -1,8 +1,4 @@
 # frozen_string_literal: true
 
 class NonDeviseUser < ApplicationUserRecord
-  if DEVISE_ORM == :mongoid
-    require './test/dummy/app/models/mongoid/mappings'
-    include ::Mongoid::Mappings
-  end
 end
