a6e9a446a7440341270b7db8479d39eb88209865
Dillon Welch <daw0328@gmail.com>
2021-10-10 22:42:32 -0400

    Remove test support for Rails 5.0 and 5.1 (#332)
    These versions are no longer supported by Rails, so per our maintenance
    policy we will no longer run tests or guarantee functionality.

--- test/orm/active_record.rb
+++ test/orm/active_record.rb
@@ -7,8 +7,6 @@ when Rails.gem_version >= Gem::Version.new('6.0.0')
   ActiveRecord::MigrationContext.new(File.expand_path('../../dummy/db/migrate', __FILE__), ActiveRecord::SchemaMigration).migrate
 when Rails.gem_version >= Gem::Version.new('5.2.0')
   ActiveRecord::MigrationContext.new(File.expand_path('../../dummy/db/migrate', __FILE__)).migrate
-else
-  ActiveRecord::Migrator.migrate(File.expand_path('../../dummy/db/migrate', __FILE__))
 end
 
 DatabaseCleaner[:active_record].strategy = :transaction
