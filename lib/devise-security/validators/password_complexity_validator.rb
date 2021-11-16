898843982908715b153a0c01fb9a0e91a649b69d
Dillon Welch <daw0328@gmail.com>
2021-11-15 21:47:46 -0500

    Fix translations for digits/symbols keys on password complexity
    The following options should add the same error message:
    * `{ digit: 1 }` and `{ digits: 1 }`
    * `{ digit: 2 }` and `{ digits: 2 }`
    * `{ symbol: 1 }` and `{ symbols: 1 }`
    * `{ symbol: 2 }` and `{ symbols: 2 }`
    
    As there were no translations for the `digits` and `symbols` keys by
    default, this would result in a missing translation error.
    
    Instead of having to add a separate locale key for `digits` and
    `symbols`, I opted to map those keys to the correspoding singular word.
    This way, as long as the singular word is translated, the plural word
    will be too with no extra effort.
    
    It could be argued that `{ digit: 2 }` and `{ symbol: 2 }` should not
    be allowed due to them being singular words with a plural count. I
    chose not to go down this route for two reasons:
    * It would add additional error handling code to maintain as well as
      throw an exception for something that otherwise would not (unless
      the user enabled a specific setting).
    * Based on the amount of contributed translation files, there are many
      non-native English speakers using the gem. This is a minor enough
      issue that they shouldn't be punished for it.

--- lib/devise-security/validators/password_complexity_validator.rb
+++ lib/devise-security/validators/password_complexity_validator.rb
@@ -4,9 +4,11 @@
 # Options:
 # - digit:  minimum number of digits in the validated string
 # - digits: minimum number of digits in the validated string
+#           uses localization string for "digit" above
 # - lower:  minimum number of lower-case letters in the validated string
 # - symbol: minimum number of punctuation characters or symbols in the validated string
 # - symbols: minimum number of punctuation characters or symbols in the validated string
+#            uses localization string for "symbol" above
 # - upper:  minimum number of upper-case letters in the validated string
 class DeviseSecurity::PasswordComplexityValidator < ActiveModel::EachValidator
   PATTERNS = {
@@ -18,13 +20,22 @@ class DeviseSecurity::PasswordComplexityValidator < ActiveModel::EachValidator
     upper: /\p{Upper}/
   }.freeze
 
+  KEYS = {
+    digits: :digit,
+    symbols: :symbol
+  }.freeze
+
   def validate_each(record, attribute, value)
     active_pattern_keys.each do |key|
       minimum = [0, options[key].to_i].max
       pattern = Regexp.new PATTERNS[key]
 
       unless (value || '').scan(pattern).size >= minimum
-        record.errors.add attribute, :"password_complexity.#{key}", count: minimum
+        record.errors.add(
+          attribute,
+          :"password_complexity.#{KEYS.fetch(key, key)}",
+          count: minimum
+        )
       end
     end
   end
