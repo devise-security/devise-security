c5d899a951b1cecdf5e9d7bf13cf3071a4be74ad
Dillon Welch <daw0328@gmail.com>
2021-12-29 05:20:51 -0500

    fix expand_path

--- lib/generators/devise_security/install_generator.rb
+++ lib/generators/devise_security/install_generator.rb
@@ -6,7 +6,7 @@ module DeviseSecurity
     class InstallGenerator < Rails::Generators::Base
       LOCALES = %w[by cs de en es fa fr hi it ja nl pt ru tr uk zh_CN zh_TW].freeze
 
-      source_root File.expand_path('../../templates', __FILE__)
+      source_root File.expand_path('../templates', __dir__)
       desc 'Install the devise security extension'
 
       def copy_initializer
