67ea0ec15c00024252a7ab3729657d7752f488ff
Dillon Welch <daw0328@gmail.com>
2021-12-29 03:57:32 -0500

    validation

--- lib/devise-security/add_banned_passwords.rb
+++ lib/devise-security/add_banned_passwords.rb
@@ -7,16 +7,16 @@ module DeviseSecurity
     end
 
     def add
-      passwords = []
-      File.foreach(@file) do |line|
-        passwords << { password: line.chomp }
-      end
-
       if Rails.gem_version >= Gem::Version.new('6.0')
+        passwords = []
+        # TODO batch over certain size limit
+        File.foreach(@file) do |line|
+          passwords << { password: line.chomp }
+        end
         BannedPassword.insert_all(passwords)
       else
-        passwords.each do |password|
-          BannedPassword.create(password)
+        File.foreach(@file) do |line|
+          BannedPassword.create(password: line.chomp)
         end
       end
     end
