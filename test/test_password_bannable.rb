9db7df259952a7e95d0a4781a20eed01837fe383
Dillon Welch <daw0328@gmail.com>
2021-12-29 02:29:29 -0500

    use new

--- test/test_password_bannable.rb
+++ test/test_password_bannable.rb
@@ -4,14 +4,14 @@ require 'test_helper'
 class TestPasswordArchivable < ActiveSupport::TestCase
   test 'user is valid if password is not banned' do
     BannedPassword.create(password: 'Password2')
-    user = User.create(email: 'bob@microsoft.com', password: 'Password1', password_confirmation: 'Password1')
+    user = User.new(email: 'bob@microsoft.com', password: 'Password1', password_confirmation: 'Password1')
 
     assert(user.valid?)
   end
 
   test 'user is invalid if password is banned' do
     BannedPassword.create(password: 'Password1')
-    user = User.create(email: 'bob@microsoft.com', password: 'Password1', password_confirmation: 'Password1')
+    user = User.new(email: 'bob@microsoft.com', password: 'Password1', password_confirmation: 'Password1')
 
     user.valid?
     assert_equal(user.errors.messages, { password: ['is banned.'] })
