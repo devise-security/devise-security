b124bb9d0ec51034bcc7c75e2b6a637fa4857357
vlad psh <vlad@shuttlerock.co.jp>
2022-08-24 19:42:26 +0900

    PasswordExpiredController: Accept block for update action

--- test/dummy/app/controllers/overrides/password_expired_controller.rb
+++ test/dummy/app/controllers/overrides/password_expired_controller.rb
@@ -1,7 +1,17 @@
 # frozen_string_literal: true
 
 class Overrides::PasswordExpiredController < Devise::PasswordExpiredController
+  def update
+    super do |resource|
+      @update_block_called = true
+    end
+  end
+
   def after_password_expired_update_path_for(_)
     '/cookies'
+  end
+
+  def update_block_called?
+    @update_block_called == true
   end
 end
