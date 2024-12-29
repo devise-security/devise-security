65016ec12bff661c6db5ba05b7a95d91a36aefac
Kevin Olbrich <kevin.olbrich@gethealthie.com>
2024-12-29 11:06:09 -0500

    fix more tests

--- test/controllers/test_security_question_controller.rb
+++ test/controllers/test_security_question_controller.rb
@@ -2,22 +2,25 @@
 
 require 'test_helper'
 
-class TestWithSecurityQuestion < ActionController::TestCase
-  include Devise::Test::ControllerHelpers
-  tests SecurityQuestion::UnlocksController
-
+class TestWithSecurityQuestion < ActionDispatch::IntegrationTest
   setup do
     @user = SecurityQuestionUser.create!(
-      username: 'hello', email: generate_unique_email, password: 'A1234567z!', security_question_answer: 'Right Answer'
+      username: 'hello',
+      email: generate_unique_email,
+      password: 'A1234567z!',
+      security_question_answer: 'Right Answer'
     )
-    @user.lock_access!
-    assert @user.locked_at.present?
-    @request.env['devise.mapping'] = Devise.mappings[:security_question_user]
+    # Starting in Rails 8.0, sending devise notification emails in setup is failing because the Devise mappings for
+    # classes haven't yet been loaded and the mailer needs them to send emails. These tests don't actually need the
+    # emails to be sent, so we can just stub the method to do nothing.
+    @user.stub :send_devise_notification, nil do
+      @user.lock_access!
+    end
   end
 
   test 'When security question is enabled, it is inserted correctly' do
     post(
-      :create,
+      '/security_question_users/unlock',
       params: {
         security_question_answer: 'wrong answer',
         security_question_user: {
@@ -25,13 +28,14 @@ class TestWithSecurityQuestion < ActionController::TestCase
         }
       }
     )
+
     assert_equal I18n.t('devise.invalid_security_question'), flash[:alert]
     assert_redirected_to new_security_question_user_unlock_path
   end
 
   test 'When security_question is valid, it runs as normal' do
     post(
-      :create,
+      '/security_question_users/unlock',
       params: {
         security_question_answer: @user.security_question_answer,
         security_question_user: {
@@ -45,20 +49,24 @@ class TestWithSecurityQuestion < ActionController::TestCase
   end
 end
 
-class TestWithoutSecurityQuestion < ActionController::TestCase
-  include Devise::Test::ControllerHelpers
-  tests Devise::UnlocksController
-
+class TestWithoutSecurityQuestion < ActionDispatch::IntegrationTest
   setup do
     @user = User.create(
-      username: 'hello', email: generate_unique_email, password: '1234', security_question_answer: 'Right Answer'
+      username: 'hello',
+      email: generate_unique_email,
+      password: '1234',
+      security_question_answer: 'Right Answer'
     )
-    @user.lock_access!
-    @request.env['devise.mapping'] = Devise.mappings[:user]
+    # Starting in Rails 8.0, sending devise notification emails in setup is failing because the Devise mappings for
+    # classes haven't yet been loaded and the mailer needs them to send emails. These tests don't actually need the
+    # emails to be sent, so we can just stub the method to do nothing.
+    @user.stub :send_devise_notification, nil do
+      @user.lock_access!
+    end
   end
 
   test 'When security question is not enabled it is not inserted' do
-    post :create, params: { user: { email: @user.email } }
+    post '/users/unlock', params: { user: { email: @user.email } }
 
     assert_equal I18n.t('devise.unlocks.send_instructions'), flash[:notice]
     assert_redirected_to new_user_session_path
